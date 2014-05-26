d1aac323df60b752d25e82ed21909505323fa1a7
Stuart Watt <stuart@morungos.com>
2014-05-26 15:45:30 -0400

    We can't use an attribute because of a Moose bug; https://rt.cpan.org/Ticket/Display.html?id=77406. Use a method instead

--- lib/Precis/LanguageTools.pm
+++ lib/Precis/LanguageTools.pm
@@ -5,16 +5,21 @@ use common::sense;
 use Moose::Role;
 use namespace::autoclean;
 
-has tools => (
-  is => 'rw'
-);
+use WordNet::QueryData;
+use Lingua::ENgenomic::Tagger;
+use Lingua::Stem::Snowball;
 
 sub BUILD {
   my ($self) = @_;
   my $wn = WordNet::QueryData->new(verbose => 0, noload => 1);
   my $tagger = Lingua::ENgenomic::Tagger->new();
   my $stemmer = Lingua::Stem::Snowball->new(lang => 'en');
-  $self->tools({wordnet => $wn, tagger => $tagger, stemmer => $stemmer});
+  $self->{_tools} = {wordnet => $wn, tagger => $tagger, stemmer => $stemmer};
+}
+
+sub tools {
+  my ($self) = @_;
+  return $self->{_tools};
 }
 
 1;
